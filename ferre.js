24a89a5571ef28858d4b1b40380ba078673d4c74
osvaldoalvarez <osvaldo_alvarez@live.com.ar>
2022-04-12 12:30:06 -0300

    creando index css y js

---
+++ ferre.js
@@ -0,0 +1,16 @@
+let nombreIngresad = prompt("Ingresar nombre");
+let apellidoIngresado = prompt("Ingresar apellido");
+
+if ((nombreIngresad != "") && (apellidoIngresado != "")) {
+    alert("Nombre: " + nombreIngresad + "\nApellido: " + apellidoIngresado);
+} else {
+    alert("Error: Ingresar nombre y apellido");
+}
+const valorDolarUsa = 120;
+var dolaresACambiar = prompt("Ingresa la cantidad de dólares que deseas cambiar (40 $U por dólar)");
+alert("Deberías recibir " + (dolaresACambiar * valorDolarUsa) + " pesos ");
+
+let cuadro = prompt("ingrese club");
+alert(cuadro + " " + "campeon");
+
+
